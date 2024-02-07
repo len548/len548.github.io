25d071e4e0c81c889a79a31f9a7ba72750a556d9
renkoike-o83aqr <63490262+len548@users.noreply.github.com>
2024-02-07 01:07:35 +0100

    updated to task2

---
+++ main.js
@@ -0,0 +1,5 @@
+window.addEventListener(("scroll"),()=>{
+    document.querySelector("#Home h1").style.marginTop = `${window.scrollY * 1.5}px`
+    document.querySelector("#Mountain1").style.marginBottom = `${106 - window.scrollY}px`
+    
+})
